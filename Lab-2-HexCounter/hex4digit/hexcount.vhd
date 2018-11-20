54c86846087aa19291fd56651937c7e534f623a6
diver-down <phalanxd@gmail.com>
2018-11-20 14:33:41 -0500

    Cleaned up the code, added credit info

--- Lab-2-HexCounter/hex4digit/hexcount.vhd
+++ Lab-2-HexCounter/hex4digit/hexcount.vhd
@@ -1,57 +1,43 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    13:09:07 10/25/2018 
--- Design Name: 
--- Module Name:    hexcount - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
--- adding change 
+-- CREDIT TO: Professor Bryan Ackland, C.D., S.J., T.J., Y.C. 
 ----------------------------------------------------------------------------------
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
-
+use IEEE.STD_LOGIC_1164.all;
 entity hexcount is
-	Port ( clk_50MHz : in STD_LOGIC;
-			anode : out STD_LOGIC_VECTOR (3 downto 0);
-			seg : out STD_LOGIC_VECTOR (6 downto 0));
+	port (
+		clk_50MHz : in STD_LOGIC;
+		anode     : out STD_LOGIC_VECTOR (3 downto 0);
+		seg       : out STD_LOGIC_VECTOR (6 downto 0));
 end hexcount;
 
 architecture Behavioral of hexcount is
-component counter is
-	Port ( clk : in STD_LOGIC;
-			 count : out STD_LOGIC_VECTOR (15 downto 0);
-		    mpx : out STD_LOGIC_VECTOR (1 downto 0));
-end component;
-component mymux is 
-Port ( sel : in STD_LOGIC_VECTOR (1 downto 0);
-		 A : in STD_LOGIC_VECTOR (15 downto 0);
-		 B : out STD_LOGIC_VECTOR (3 downto 0));
-end component;
+	component counter is
+		port (
+			clk   : in STD_LOGIC;
+			count : out STD_LOGIC_VECTOR (15 downto 0);
+			mpx   : out STD_LOGIC_VECTOR (1 downto 0));
+	end component;
+	component mymux is
+		port (
+			sel : in STD_LOGIC_VECTOR (1 downto 0);
+			A   : in STD_LOGIC_VECTOR (15 downto 0);
+			B   : out STD_LOGIC_VECTOR (3 downto 0));
+	end component;
 
-component leddec is
-Port ( mpx : in STD_LOGIC_VECTOR (1 downto 0); 
-		data : in STD_LOGIC_VECTOR (3 downto 0);
-		anode : out STD_LOGIC_VECTOR (3 downto 0);
-		seg : out STD_LOGIC_VECTOR (6 downto 0));
-end component;
+	component leddec is
+		port (
+			mpx   : in STD_LOGIC_VECTOR (1 downto 0);
+			data  : in STD_LOGIC_VECTOR (3 downto 0);
+			anode : out STD_LOGIC_VECTOR (3 downto 0);
+			seg   : out STD_LOGIC_VECTOR (6 downto 0));
+	end component;
 
-signal F : STD_LOGIC_VECTOR (1 downto 0);
-signal S : STD_LOGIC_VECTOR (15 downto 0);
-signal data_1 : STD_LOGIC_VECTOR (3 downto 0);
+	signal F      : STD_LOGIC_VECTOR (1 downto 0);
+	signal S      : STD_LOGIC_VECTOR (15 downto 0);
+	signal data_1 : STD_LOGIC_VECTOR (3 downto 0);
 
-begin 
-C1 : counter port map (clk=>clk_50MHz, count=>S, mpx => F);
-L1 : leddec port map (mpx => F, data=>data_1, anode=>anode, seg=>seg);
-mux : mymux port map (sel => F, A => S, B => data_1);
+begin
+	C1  : counter port map(clk => clk_50MHz, count => S, mpx => F);
+	L1  : leddec port map(mpx => F, data => data_1, anode => anode, seg => seg);
+	mux : mymux port map(sel => F, A => S, B => data_1);
 end Behavioral;
--- Lab-2-HexCounter/hex4digit/leddec.vhd
+++ Lab-2-HexCounter/hex4digit/leddec.vhd
@@ -1,54 +1,39 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    12:36:40 10/25/2018 
--- Design Name: 
--- Module Name:    leddec - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
+-- CREDIT TO: Professor Bryan Ackland, C.D., S.J., T.J., Y.C.
 ----------------------------------------------------------------------------------
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_1164.all;
 entity leddec is
- Port ( mpx : in STD_LOGIC_VECTOR (1 downto 0);
- data : in STD_LOGIC_VECTOR (3 downto 0);
- anode: out STD_LOGIC_VECTOR (3 downto 0);
- seg : out STD_LOGIC_VECTOR (6 downto 0));
+	port (
+		mpx   : in STD_LOGIC_VECTOR (1 downto 0);
+		data  : in STD_LOGIC_VECTOR (3 downto 0);
+		anode : out STD_LOGIC_VECTOR (3 downto 0);
+		seg   : out STD_LOGIC_VECTOR (6 downto 0));
 end leddec;
 architecture Behavioral of leddec is
 begin
--- Turn on segments corresponding to 4-bit data word
-seg <= "0000001" when data="0000" else --0
-"1001111" when data="0001" else --1
-"0010010" when data="0010" else --2
-"0000110" when data="0011" else --3
-"1001100" when data="0100" else --4
-"0100100" when data="0101" else --5
-"0100000" when data="0110" else --6
-"0001111" when data="0111" else --7
-"0000000" when data="1000" else --8
-"0000100" when data="1001" else --9
-"0001000" when data="1010" else --A
-"1100000" when data="1011" else --B
-"0110001" when data="1100" else --C
-"1000010" when data="1101" else --D
-"0110000" when data="1110" else --E
-"0111000" when data="1111" else --F
-"1111111";
--- Turn on anode of 7-segment display addressed by 2-bit digit selector dig
-anode <= "1110" when mpx="00" else --0
-			"1101" when mpx="01" else --1
-			"1011" when mpx="10" else --2
-			"0111" when mpx="11" else --3		
-			"1111";
+	-- Turn on segments corresponding to 4-bit data word
+	seg <= "0000001" when data = "0000" else --0
+		"1001111" when data = "0001" else --1
+		"0010010" when data = "0010" else --2
+		"0000110" when data = "0011" else --3
+		"1001100" when data = "0100" else --4
+		"0100100" when data = "0101" else --5
+		"0100000" when data = "0110" else --6
+		"0001111" when data = "0111" else --7
+		"0000000" when data = "1000" else --8
+		"0000100" when data = "1001" else --9
+		"0001000" when data = "1010" else --A
+		"1100000" when data = "1011" else --B
+		"0110001" when data = "1100" else --C
+		"1000010" when data = "1101" else --D
+		"0110000" when data = "1110" else --E
+		"0111000" when data = "1111" else --F
+		"1111111";
+	-- Turn on anode of 7-segment display addressed by 2-bit digit selector dig
+	anode <= "1110" when mpx = "00" else --0
+		"1101" when mpx = "01" else --1
+		"1011" when mpx = "10" else --2
+		"0111" when mpx = "11" else --3		
+		"1111";
 end Behavioral;
