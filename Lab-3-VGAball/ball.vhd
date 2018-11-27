1116cd1dbaa70bfa0f68dcab3d1333877bd3426e
diver-down <cdrew2@stevens.edu>
2018-11-27 11:46:09 -0500

    Change color and size of ball

--- Lab-3-VGAball/ball.vhd
+++ Lab-3-VGAball/ball.vhd
@@ -17,7 +17,7 @@ entity ball is
 end ball;
 
 architecture Behavioral of ball is
-	constant size        : integer := 8;
+	constant size        : integer := 20;
 	signal ball_on       : STD_LOGIC; -- indicates whether ball is over current pixel position
 	-- current ball position - intitialized to center of screen
 	signal ball_x        : STD_LOGIC_VECTOR(9 downto 0) := CONV_STD_LOGIC_VECTOR(320, 10);
@@ -26,20 +26,23 @@ architecture Behavioral of ball is
 	signal ball_x_motion : STD_LOGIC_VECTOR(9 downto 0) := "0000000100";
 	signal ball_y_motion : STD_LOGIC_VECTOR(9 downto 0) := "0000000100";
 begin
-	red   <= '1'; -- color setup for red ball on white background
-	green <= not ball_on;
+	red   <= not ball_on; -- color setup for green ball on white background
+	green <= '1';
 	blue  <= not ball_on;
 	-- process to draw ball current pixel address is covered by ball position
 	bdraw : process (ball_x, ball_y, pixel_row, pixel_col) is
 	begin
-		if (pixel_col >= ball_x - size) and
-			(pixel_col <= ball_x + size) and
-			(pixel_row >= ball_y - size) and
-			(pixel_row <= ball_y + size) then
-			ball_on    <= '1';
-		else
-			ball_on <= '0';
-		end if;
+		--if (pixel_col >= ball_x - size) and
+			--(pixel_col <= ball_x + size) and
+			--(pixel_row >= ball_y - size) and
+			--(pixel_row <= ball_y + size)
+			-- commented-out original square-shape code above
+		if((conv_integer(pixel_col) - conv_integer(ball_x))*(conv_integer(pixel_col) - conv_integer(ball_x)) + (conv_integer(pixel_row) - conv_integer(ball_y))*(conv_integer(pixel_row) - conv_integer(ball_y)) <= size * size)
+			then
+				ball_on <= '1';
+			else
+				ball_on <= '0';
+			end if;
 	end process;
 	-- process to move ball once every frame (i.e. once every vsync pulse)
 	mball : process
